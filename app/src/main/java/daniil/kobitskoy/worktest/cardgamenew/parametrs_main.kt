ee1a740a1cbdaff4045b09d0e45b99b9673b0184
dkobi <dkobitskoy123@mail.ru>
2022-11-17 22:22:54 +0300

    push

--- app/src/main/java/daniil/kobitskoy/worktest/cardgamenew/parametrs_main.kt
+++ app/src/main/java/daniil/kobitskoy/worktest/cardgamenew/parametrs_main.kt
@@ -1,7 +1,7 @@
 package daniil.kobitskoy.worktest.cardgamenew
 
 object parametrs_main {
-    const val ONESIGNAL_KEY = "abab8392-8cf4-4943-b866-42e7ed5907b4"
+    const val ONESIGNAL_KEY = "c8c4032d-82d0-42e5-a216-936e7712b7c8"
 //    const val APPSFLYER_KEY = "4Ljqb4F2tW2jW7CddMKz6N"
     const val APPSFLYER_KEY = "dK62WkhogfXe6EkdqcC3DN"
     var response : MutableMap<String, Any>? = null
